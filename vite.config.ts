accb7cc03ec3616ccb158102b572df07a9bfdaf8
jdaly952 <jdaly952@gmail.com>
2026-04-18 14:44:06 -0500

    fix: production ready config

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import {defineConfig, loadEnv} from 'vite';
-
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
@@ -17,10 +16,10 @@ export default defineConfig(({mode}) => {
     },
     server: {
       hmr: process.env.DISABLE_HMR !== 'true',
-      port: parseInt(process.env.PORT || '3000'),
+      port: parseInt(process.env.PORT || '8080'),
     },
     preview: {
-      port: parseInt(process.env.PORT || '3000'),
+      port: parseInt(process.env.PORT || '8080'),
     },
   };
 });
